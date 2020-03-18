702469a9eac617a06cd73d81f90c57c7c97e1358
Azlan <vhussein80@gmail.com>
2020-03-18 23:07:49 +0800

    - Added swagger capability - updated the readme for using the demo project

--- src/main/java/com/azlan/test/TestMainApplication.java
+++ src/main/java/com/azlan/test/TestMainApplication.java
@@ -3,15 +3,32 @@ package com.azlan.test;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Slf4j
 @EnableTransactionManagement
 @SpringBootApplication
+@EnableSwagger2
 public class TestMainApplication {
 
     public static void main(String[] args){
         log.debug("This is a test client");
         SpringApplication.run(TestMainApplication.class, args);
     }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
 }
