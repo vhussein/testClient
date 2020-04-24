5dcb25028b0e4c855a9be26d7e5eed5ed0fe71f2
Azlan <vhussein80@gmail.com>
2020-04-24 15:08:20 +0800

    enhancements based from feedback

--- src/main/java/com/azlan/test/controller/ClientController.java
+++ src/main/java/com/azlan/test/controller/ClientController.java
@@ -2,13 +2,16 @@ package com.azlan.test.controller;
 
 import com.azlan.test.exception.ResourceNotFoundException;
 import com.azlan.test.model.Client;
+import com.azlan.test.model.ResponseModel;
 import com.azlan.test.service.ClientService;
 import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -28,24 +31,24 @@ public class ClientController {
             @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
             @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found") })
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Client> getClients() {
         log.debug("Get all client list");
         return clientService.getClients();
     }
 
     @ApiOperation(value = "Get a client by First Name")
-    @GetMapping("/getClientByFirstName")
-    public ResponseEntity<List<Client>> getClientByFirstName(@RequestBody Client client) throws ResourceNotFoundException {
+    @GetMapping(value = "/getClientByFirstName", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Client>> getClientByFirstName(@RequestParam("name") String name) throws ResourceNotFoundException {
         log.debug("Get client by first name");
-        List<Client> clientList = clientService.findByFirstName(client.getFirstName());
+        List<Client> clientList = clientService.findByFirstName(name);
         if(clientList.isEmpty())
-            throw new ResourceNotFoundException("Unable to find client with first name: " + client.getFirstName());
+            throw new ResourceNotFoundException("Unable to find client with first name: " + name);
         return ResponseEntity.ok().body(clientList);
     }
 
     @ApiOperation(value = "Get a client by Id")
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Client> getClient(
             @ApiParam(value = "Client id from which client object will retrieve", required = true)
             @PathVariable("id") final Long id) throws ResourceNotFoundException {
@@ -57,25 +60,25 @@ public class ClientController {
     }
 
     @ApiOperation(value = "Create a client")
-    @PostMapping
-    public ResponseEntity<String> createClient(@RequestBody  Client client) {
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseModel> createClient(@RequestBody  Client client) {
 
         try{
             log.debug("Creating client");
             clientService.createClient(client);
             return ResponseEntity.status(HttpStatus.OK)
-                    .body("Client " + client.getFirstName() + " " + client.getLastName() + " created");
+                    .body(new ResponseModel(new Date(), "Client " + client.getFirstName() + " " + client.getLastName() + " created", ""));
         }
         catch (Exception ex){
             log.error("Error: " + ex.toString());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("");
+                    .body(new ResponseModel(new Date(), "Error occured", ""));
         }
     }
 
-    @GetMapping(value = "/healthCheck")
-    public ResponseEntity<String> healthCheck(){
-        return ResponseEntity.status(HttpStatus.OK).body("I'm alive");
+    @GetMapping(value = "/healthCheck", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseModel> healthCheck(){
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseModel(new Date(), "I'm alive", ""));
     }
 
 }
